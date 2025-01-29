1ff512c8a1b5ba48bd3e1743a1961ee8f8d1083c
Juan Camilo Suárez <ccamsoto@gmail.com>
2025-01-28 22:52:48 -0500

    circle linked list were applied

--- structured_data/nodes/circleLinkedList.py
+++ structured_data/nodes/circleLinkedList.py
@@ -1,10 +1,35 @@
-"""
-create an array 
-asign the a linkedlist the array
-"""
-class Arrays():
-    def __init__(self, size, fill_values= None):
-        self.items = list()
-        for _ in range(size):
-            self.items.append(fill_values)
+# create a cyrcular node structure 
+class singleNode():
+    """
+    una lista enlazada simple, la estructura es lineal, no permite 
+    múltiples conexiones directas desde un solo nodo a varios nodos.
+    """
+    def __init__(self, value, next =None):
+        self.value = value
+        self.next = next
+    def __str__(self):
+        return self.value
+
+class CircularlinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0 #it starts with it
         
+    def addNodes(self, value):
+        """
+        if: self.size or self.time are empty, create the head node
+        else: traverse the linked list until find the last node; there, the new node will be created 
+        """
+        node = singleNode(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.size +=1
+    
+    def size(self):
+        return str(self.size)
+  
